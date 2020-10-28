48e1760a9255aab781c2b4a0ada16244c49fa143
rigonava14 <71853131+rigonava14@users.noreply.github.com>
2020-10-28 16:26:44 -0700

    Repaso Final

---
+++ Repaso/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int arreglo1[]= {2,3,8,7,1};
+        int suma=0;
+        for (int x=0;x<arreglo1.length;x++){
+
+            suma = suma+arreglo1[x];
+
+        }
+        System.out.println(suma);
+    }
+}
